aea042ef9622372d85dfd6f54f0cfc6033af9e15
somporn.s <sompor222@gmail.com>
2023-08-15 23:58:12 +0700

    เพิ่ม header footer

---
+++ src/app/(front)/page.tsx
@@ -0,0 +1,31 @@
+
+export default function Home() {
+  return (
+    <main className="">
+      bodys
+      <p>s</p>
+      <p>s</p>
+      <p>s</p>
+      <p>s</p>
+      <p>s</p>
+      <p>s</p>
+      <p>s</p>
+      <p>s</p>
+      <p>s</p>
+      <p>s</p>
+      <p>s</p>
+      <p>s</p>
+      <p>s</p>
+      <p>s</p>
+      <p>s</p>
+      <p>s</p>
+      <p>s</p>
+      <p>s</p>
+      <p>s</p>
+      <p>s</p>
+      <p>s</p>
+      <p>s</p>
+      <p>s</p>
+    </main>
+  )
+}
